0075ed8e86bd7070a0f468c8c65db68e8a98a937
wsp8521 <wsp8521@hotmail.com>
2024-12-02 15:26:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from componet.interface import Component
 from chat.chatboot_groq import ChatbootGroq
